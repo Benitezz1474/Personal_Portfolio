b750e1c9efaa2c89ef514739a5f1911ea28eec4a
Benitez Fernando <benitezz1474@outlook.com>
2023-02-27 20:46:26 -0300

    V3

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return <>
        <h1>En construcción</h1>
